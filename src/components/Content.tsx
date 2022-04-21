4d8c2e9fa34f42b5e2c4dd0f24dbd1c8c923983c
francielsb <francielsb@gmail.com>
2022-04-21 16:52:28 -0300

    Ajuste no componente do Content

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,40 @@
-export function Content() {
+import { MovieCard } from './MovieCard';
+
+import '../styles/content.scss';
+
+interface Movie {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface ContentProps {
+  movies: Movie[];
+  title: string;  
+}
+
+export function Content(props: ContentProps) {
   // Complete aqui
+  return (
+    <>
+      <div className="container">
+        <header>
+          <span className="category">Categoria:<span> {props.title}</span></span>
+        </header>
+
+        <main>
+          <div className="movies-list">
+            {props.movies.map(movie => (
+              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+            ))}
+          </div>
+        </main>
+      </div>
+    </>
+  );
 }
