1d6271cb2f3d3f2fa7d7c6c2096bd3445baf1ee0
francielsb <francielsb@gmail.com>
2022-04-21 16:52:14 -0300

    Ajuste no componente do SideBar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,44 @@
-export function SideBar() {
+import { Dispatch, SetStateAction } from 'react';
+import { Button } from './Button';
+
+import '../styles/sidebar.scss';
+
+interface Genre {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface SideBarProps {
+  genres: Genre[];
+  selectedGenreId: number;
+  setSelectedGenreId: Dispatch<SetStateAction<number>>;  
+}
+
+export function SideBar(props: SideBarProps) {
+  function handleClickButton(id: number) {
+    props.setSelectedGenreId(id);
+  }
+
   // Complete aqui
+  return (
+    <>
+      <nav className="sidebar">
+          <span>Watch<p>Me</p></span>
+
+          <div className="buttons-container">
+            {props.genres.map(genre => (
+              <Button
+                key={String(genre.id)}
+                title={genre.title}
+                iconName={genre.name}
+                onClick={() => handleClickButton(genre.id)}
+                selected={props.selectedGenreId === genre.id}
+              />
+            ))}
+          </div>
+
+        </nav>
+    </> 
+  );
 }
